abd6e19003b4dabf74e320fce1ac8c64306f374c
jinshan <jinshan@hummergames.com>
2021-03-15 23:11:21 +0800

    Update Common.py

--- Common.py
+++ Common.py
@@ -2,9 +2,16 @@
 import hashlib
 import subprocess
 import sys
+import os
+import argparse
 
 G_ZIP_SPLIT_LINE = 500
 G_ZIP_SPLIT_UNIT = 100
+
+
+def os_system(cmd, use_secure = False):
+    print "cmd", cmd
+    os.system(cmd)
 
 
 def os_popen(cmd):
@@ -19,26 +26,45 @@ def os_popen(cmd):
         sys.exit(1)
 
 
+def read_file(filename, mode="r"):
+    with open(filename, mode) as f_read:
+        buff = f_read.read()
+        return buff
+
+
+def save_file(filename, content, mode="w"):
+    with open(filename, mode) as f_write:
+        f_write.write(content)
+
+
 def _md5(data):
-    md5 = hashlib.md5(data).hexdigest()
-    return md5
+    _hash = hashlib.md5(data).hexdigest()
+    return _hash
 
 
-def md5_file(filepath):
+def md5(filepath):
     path = filepath.replace("\\", "/")
     print(path)
     with open(path, "rb") as f_read:
         buff = f_read.read()
         if len(buff) > 0:
-            md5 = _md5(buff)
-            return md5
+            _hash = _md5(buff)
+            return _hash
         else:
             return ""
 
 
+def zip_dir(name, dirpath):
+    assert isinstance(dirpath, str), "error: dirpath={0}".format(dirpath)
+    os.chdir(dirpath)
+    files = []
+    for name in os.listdir("."):
+        files.append(os.path.join(dirpath, name))
+    zip_files(name, files)
+
+
 def zip_files(name, filepath):
-    if not isinstance(filepath, list):
-        return
+    assert isinstance(filepath, list), "error: filepath={0}".format(filepath)
     zip_len = len(filepath)
     if zip_len <= 0:
         return
